f067d10d32f1205cfc7e10c714a12419124807d3
root <root@ywnk>
2017-01-13 12:49:11 +0100

    question2 manque plus que la durée

--- question2.sql
+++ question2.sql
@@ -5,9 +5,9 @@ BEGIN
 	FOR ligne
 	IN (SELECT * FROM patient INNER JOIN souffre ON patient.id_patient=souffre.id_patient INNER JOIN 
 maladie ON souffre.id_maladie=maladie.id_maladie INNER JOIN necessite ON maladie.id_maladie=necessite.id_maladie
-INNER JOIN traitement ON necessite.id_traitement=traitement.id_traitement where patient.id_patient=idPatient)
+INNER JOIN traitement ON necessite.id_traitement=traitement.id_traitement INNER JOIN constitue ON traitement.id_traitement=constitue.id_traitement INNER JOIN medicament ON constitue.id_medicament=medicament.id_medicament where patient.id_patient=idPatient)
 	LOOP
-		dbms_output.put_line(ligne,ligne);	
+		dbms_output.put_line(ligne.libelle);	
 	END LOOP;
 END; 
 /
